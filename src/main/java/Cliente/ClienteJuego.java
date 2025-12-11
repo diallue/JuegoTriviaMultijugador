49efb214dff20ee9019e8530fca77d9065eec9f2
diallue <diallue@unirioja.es>
2025-12-11 10:31:30 +0100

    final commit

--- src/main/java/Cliente/ClienteJuego.java
+++ src/main/java/Cliente/ClienteJuego.java
@@ -12,7 +12,7 @@ public class ClienteJuego {
     public void conectarAlServidor(String host, int puerto) {
         try {
             // Conectar al servidor
-            System.setProperty("javax.net.ssl.trustStore", "ksrc/main/resources/keystore.p12");
+            System.setProperty("javax.net.ssl.trustStore", "src/main/resources/keystore.p12");
             System.setProperty("javax.net.ssl.trustStorePassword", "123456");
 
             SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
